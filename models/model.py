2bd1047aaf8476051271e92a8aef9740e68c4d98
Ashwin Sankar <ashwins1211@gmail.com>
2024-05-17 09:54:52 +0530

    Added code

---
+++ models/model.py
@@ -0,0 +1,179 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Encoder(nn.Module):
+    """
+    Encoder module for sequence-to-sequence models.
+
+    Args:
+        input_dim (int): The size of the input vocabulary.
+        hidden_dim (int): The number of features in the hidden state of the RNN/LSTM/GRU.
+        num_layers (int): Number of recurrent layers.
+        embedding_size (int): The size of the word embeddings.
+        bidirectional (bool): If True, the RNN/LSTM/GRU layers will be bidirectional.
+        cell_type (str): Type of recurrent cell to use. Options: 'RNN', 'LSTM', or 'GRU'.
+        dp (float): Dropout probability to use in the RNN/LSTM/GRU layers.
+
+    Raises:
+        ValueError: If `cell_type` is not one of ['RNN', 'LSTM', 'GRU'].
+
+    """
+
+
+    def __init__(self, input_dim, hidden_dim,embedding_size, num_layers, bidirectional, cell_type, dp):
+        super(Encoder, self).__init__()
+
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.embedded_size = embedding_size
+        self.cell_type = cell_type
+        self.bidirectional=bidirectional
+        self.dropout = nn.Dropout(dp)
+
+        if bidirectional:
+            self.direction=2
+        else:
+            self.direction=1  
+        
+        self.embedding_layer = nn.Embedding(input_dim,embedding_size)
+
+        if cell_type == 'RNN':
+              self.rnn = nn.RNN(embedding_size, hidden_dim, num_layers, dropout=dp,bidirectional=bidirectional)
+        elif cell_type == 'LSTM':
+              self.rnn = nn.LSTM(embedding_size, hidden_dim, num_layers, dropout=dp,bidirectional=bidirectional)
+        elif cell_type == 'GRU':
+              self.rnn = nn.GRU(embedding_size, hidden_dim, num_layers, dropout=dp,bidirectional=bidirectional)
+        else:
+            raise ValueError("Only valid cell types are: RNN, LSTM and GRU")     
+
+    def forward(self, src):
+        embedded_out = self.dropout(self.embedding_layer(src))
+        # For cell type LSTM, returns the output and the hidden and the cell states in a single tuple
+        if self.cell_type == 'LSTM':
+            output, (hidden, cell) = self.rnn(embedded_out)
+            return output, (hidden, cell)
+
+        else:
+            output, hidden = self.rnn(embedded_out) # For RNN or GRU returns the output and the hidden state
+            return output,hidden
+        
+        
+class Decoder(nn.Module):
+    """
+    Decoder module for sequence-to-sequence models.
+
+    Args:
+        output_dim (int): The size of the output vocabulary.
+        hidden_dim (int): The number of features in the hidden state of the RNN/LSTM/GRU.
+        embedding_size (int): The size of the word embeddings.
+        num_layers (int): Number of recurrent layers.
+        bidirectional (bool): If True, the RNN/LSTM/GRU layers will be bidirectional.
+        cell_type (str): Type of recurrent cell to use. Options: 'RNN', 'LSTM', or 'GRU'.
+        dp (float): Dropout probability to use in the RNN/LSTM/GRU layers.
+
+    Raises:
+        ValueError: If `cell_type` is not one of ['RNN', 'LSTM', 'GRU'].
+    """
+
+    def __init__(self, output_dim, hidden_dim, embedding_size, num_layers, bidirectional, cell_type, dp):
+        super(Decoder, self).__init__()
+        print ('Output_dim: ', output_dim)
+        print ('Hidden_dim: ', hidden_dim)
+        print ('Embedding_size: ', embedding_size)
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.embedded_size=embedding_size     
+        self.num_layers = num_layers
+        self.cell_type = cell_type
+        self.bidirectional=bidirectional
+        self.dropout = nn.Dropout(dp)
+        
+        if bidirectional:
+            self.direction=2
+        else:
+            self.direction=1  
+
+        self.embedding_layer = nn.Embedding(output_dim,embedding_size)
+        print ('output_dim: ', output_dim, hidden_dim, embedding_size)
+        if cell_type == 'RNN':
+            self.rnn = nn.RNN(embedding_size, hidden_dim, num_layers,dropout=dp)
+        elif cell_type == 'LSTM':
+            self.rnn = nn.LSTM(embedding_size, hidden_dim, num_layers,dropout=dp)
+        elif cell_type == 'GRU':
+            self.rnn = nn.GRU(embedding_size, hidden_dim, num_layers,dropout=dp)
+        else:
+            raise ValueError("Only valid cell types are: RNN, LSTM and GRU")
+        
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+        
+    def forward(self, input, hidden):
+        embedded_out = self.dropout(self.embedding_layer(input))
+        output, hidden = self.rnn(embedded_out, hidden)
+        output = self.fc_out(output)  # fully connected layer
+        output = F.log_softmax(output, dim=1) # softmax for classification
+        return output, hidden
+
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, encoder, decoder, cell_type, bidirectional, teacher_forcing_ratio, device):
+        super(EncoderDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.cell_type=cell_type
+        self.bidirectional=bidirectional
+        self.teacher_forcing_ratio = teacher_forcing_ratio
+        self.device = device
+        
+    def forward(self, src, tgt, teacher_forcing_ratio):
+        batch_size = tgt.shape[1]
+        max_len = tgt.shape[0]
+        tgt_vocab_size = self.decoder.output_dim
+        
+        outputs = torch.zeros(max_len, batch_size, tgt_vocab_size).to(device)  #shape of the output is [max_len, batch_size, tgt_vocab_size]
+        
+        encoder_output, encoder_hidden = self.encoder(src)
+
+        if self.bidirectional:
+            if self.cell_type=='LSTM':
+                hidden_states, cell_states = encoder_hidden
+                forward_hidden = hidden_states[:self.encoder.num_layers, :, :]
+                backward_hidden = hidden_states[self.encoder.num_layers:, :, :]
+
+                forward_cell = cell_states[:self.encoder.num_layers, :, :]
+                backward_cell = cell_states[self.encoder.num_layers:, :, :]
+
+                avg_hidden = (forward_hidden + backward_hidden) / 2
+                avg_cell = (forward_cell + backward_cell) / 2
+
+                hidden_concat = (avg_hidden, avg_cell)
+
+            else:
+                hidden_concat = (encoder_hidden[0:self.encoder.num_layers,:,:] + encoder_hidden[self.encoder.num_layers:,:,:])/2
+        else:
+            hidden_concat = encoder_hidden
+        
+        decoder_hidden = hidden_concat
+        # Initialize decoder input with the start token
+        decoder_input = (tgt[0,:]).unsqueeze(0)
+
+        for t in range(1,tgt.shape[0]):
+            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+            outputs[t] = decoder_output
+            max_pr, idx = torch.max(decoder_output,dim=2)
+            idx=idx.view(tgt.shape[1])
+            
+            
+            # Determine the next decoder input using teacher forcing or predicted output
+            teacher_force = teacher_forcing_ratio > torch.rand(1)
+            if teacher_force:
+                decoder_input= tgt[t,:].unsqueeze(0)
+            else:
+                decoder_input= idx.unsqueeze(0)
+
+        decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+        
+        return outputs
---
+++ models/utils.py
@@ -0,0 +1,113 @@
+
+import pandas as pd
+from copy import deepcopy
+import random
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Vocab:
+    def __init__(self, file_path, src_lang, tgt_lang):
+        self.data = pd.read_csv(file_path, sep = ',', header = None, names = [src_lang, tgt_lang])
+        self.data.dropna()
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
+        src_chars = sorted(set(''.join(self.data[src_lang])))
+        tgt_chars = sorted(set(''.join(self.data[tgt_lang])))
+
+        self.src_char_to_idx = {src_chars[i]:i+3 for i in range(len(src_chars))}
+        self.src_char_to_idx['<'] = 0
+        self.src_char_to_idx['<pad>'] = 1
+        self.src_char_to_idx['<unk>'] = 2
+
+        self.src_vocab = {src_chars[i]:i+3 for i in range(len(src_chars))}
+        self.src_vocab['<'] = 0
+        self.src_vocab['<pad>'] = 1
+        self.src_vocab['<unk>'] = 2
+        
+
+        
+        self.src_idx_to_char = {idx: char for char, idx in self.src_char_to_idx.items()}
+ 
+        self.tgt_char_to_idx =  {tgt_chars[i]:i+3 for i in range(len(tgt_chars))}
+        self.tgt_char_to_idx['<'] = 0
+        self.tgt_char_to_idx['<pad>'] = 1
+        self.tgt_char_to_idx['<unk>'] = 2
+
+        self.tgt_vocab =  {tgt_chars[i]:i+3 for i in range(len(tgt_chars))}
+        self.tgt_vocab['<'] = 0
+        self.tgt_vocab['<pad>'] = 1
+        self.tgt_vocab['<unk>'] = 2
+
+
+        self.tgt_idx_to_char = {idx: char for char, idx in self.tgt_char_to_idx.items()}
+        # self.src_vocab = (self.src_char_to_idx)
+        # self.tgt_vocab = (self.tgt_char_to_idx)
+        print (len(self.src_vocab), len(self.tgt_vocab))
+
+    def get(self):
+        return self.src_vocab, self.tgt_vocab, self.src_char_to_idx, self.src_idx_to_char, self.tgt_char_to_idx, self.tgt_idx_to_char 
+
+
+class TransliterationDataLoader(Dataset):
+    def __init__(self, filename, src_lang, tgt_lang, src_vocab, tgt_vocab, src_char_to_idx, tgt_char_to_idx):
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
+        self.src_vocab = src_vocab
+        self.tgt_vocab = tgt_vocab
+        self.src_char_to_idx = src_char_to_idx
+        self.tgt_char_to_idx = tgt_char_to_idx
+        self.start_of_word = 0
+        self.data = pd.read_csv(filename, sep = ',', header = None, names = [self.src_lang, self.tgt_lang])
+        
+        self.src_dim = max([len(word) for word in self.data[self.src_lang].values]) + 1
+        self.tgt_dim = max([len(word) for word in self.data[self.tgt_lang].values]) + 1
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        """
+        Retrieves a sample from the dataset based on the given index.
+
+        Args:
+            idx (int): Index of the sample to retrieve.
+
+        Returns:
+            tuple: Tuple containing the English word and Hindi word.
+        """
+
+        # print (self.data.head(5))
+        # print (self.data.columns)
+        # print (idx)
+        src_word = self.data.iloc[idx][self.src_lang]
+        tgt_word = self.data.iloc[idx][self.tgt_lang]
+
+        # print (src_word, tgt_word)
+        # src_word = self.data.loc[self.src_lang, idx]
+        # tgt_word = self.data.loc[self.tgt_lang, idx]
+
+        src_indices = [self.src_vocab.get(char, self.src_vocab['<unk>']) for char in src_word]
+        tgt_indices = [self.tgt_vocab.get(char, self.tgt_vocab['<unk>']) for char in tgt_word]
+
+        src_indices.insert(0, self.start_of_word)
+        tgt_indices.insert(0, self.start_of_word)
+        
+        src_len = len(src_indices)
+        tgt_len = len(tgt_indices)
+        
+        src_pad = [self.src_vocab['<pad>']] * (self.src_dim - src_len)
+        tgt_pad = [self.tgt_vocab['<pad>']] * (self.tgt_dim - tgt_len)
+
+        src_indices.extend(src_pad)
+        tgt_indices.extend(tgt_pad)
+
+        src_tensor = torch.LongTensor(src_indices)
+        tgt_tensor = torch.LongTensor(tgt_indices)
+
+        return src_tensor, tgt_tensor, src_len, tgt_len
+
+
+
